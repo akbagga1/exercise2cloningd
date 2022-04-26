4c61a9022dcd1e779582cb8122d706e34af274c4
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-26 01:03:40 -0400

    update-cloned repo, created branch, pushed updated branch.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My name is Annie Bagga.");
+        System.out.println("My name is Annie Bagga. I cloned exercise2cloningd repo, added a new branch and pushed the branch update.");
     }
 }
